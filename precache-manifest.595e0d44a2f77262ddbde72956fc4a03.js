6da8ae7604a89b44478ed9b849ec5885876035d9
suman <suman.kunwar@daliaresearch.com>
2020-01-08 19:04:40 +0100

    Updates

---
+++ precache-manifest.595e0d44a2f77262ddbde72956fc4a03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c031233569ea6577c82f871399fd4104",
+    "url": "/realtime-html-server/index.html"
+  },
+  {
+    "revision": "6d5ea19110b3d22830c8",
+    "url": "/realtime-html-server/static/css/main.968aa4e1.chunk.css"
+  },
+  {
+    "revision": "e6f379dca47fa5c191a0",
+    "url": "/realtime-html-server/static/js/2.e177046a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/realtime-html-server/static/js/2.e177046a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6d5ea19110b3d22830c8",
+    "url": "/realtime-html-server/static/js/main.f97d4ec1.chunk.js"
+  },
+  {
+    "revision": "8fb6b79fdd36422c2439",
+    "url": "/realtime-html-server/static/js/runtime-main.92526db6.js"
+  }
+]);
