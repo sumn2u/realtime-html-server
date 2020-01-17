08460d443f2b681243f1c18a84135883b1e6d752
suman <suman.kunwar@daliaresearch.com>
2020-01-17 22:48:53 +0100

    feat: syntax error added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import React, {Component} from 'react';
 import ReactDOM from "react-dom";
 import AceEditor from "react-ace";
+
 import './App.css';
 
+import "ace-builds/src-min-noconflict/mode-javascript";
+import "ace-builds/src-min-noconflict/theme-tomorrow_night_eighties";
+import "ace-builds/src-min-noconflict/ext-language_tools";
+import "ace-builds/src-min-noconflict/ext-spellcheck";
+import "ace-builds/src-min-noconflict/snippets/javascript";
+import 'ace-builds/src-min-noconflict/ext-searchbox';
+import "ace-builds/webpack-resolver";
 import "ace-builds/src-min-noconflict/ext-language_tools";
 import "ace-builds/src-noconflict/mode-html";
+// import "ace-builds/src-noconflict/worker-html";
+// import "ace-builds/src-noconflict/worker-javascript";
 import "ace-builds/src-noconflict/snippets/html";
 import "ace-builds/src-noconflict/theme-monokai";
+
+const ace = require('ace-builds/src-noconflict/ace');
+ace.config.set("basePath", "https://cdn.jsdelivr.net/npm/ace-builds@1.4.3/src-noconflict/");
+ace.config.setModuleUrl('ace/mode/javascript_worker', "https://cdn.jsdelivr.net/npm/ace-builds@1.4.3/src-noconflict/worker-javascript.js");
 
 
 class App extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      value:""
+    }
+
     this.myRef = React.createRef();
     this.onEditorChange = this.onEditorChange.bind(this)
+    this.onPaste = this.onPaste.bind(this);
   }
 
   onEditorChange(newValue) {
-    ReactDOM.findDOMNode(this.myRef.current).innerHTML = newValue
+     ReactDOM.findDOMNode(this.myRef.current).innerHTML = newValue
+  }
+
+  onPaste(event){
+    this.setState({
+      value: event.text
+    })
   }
 
   render() {
+    const {value} = this.state;
     return (
       <div className="App">
         <div className="flex-split">
@@ -33,6 +59,7 @@ class App extends Component {
                 width={"100%"}
                 ref="aceEditor"
                 onChange={this.onEditorChange}
+                onPaste= {this.onPaste}
                 fontSize={14}
                 showPrintMargin={true}
                 focus={true}
@@ -40,7 +67,7 @@ class App extends Component {
                 wrapEnabled= {true}
                 highlightActiveLine={true}
                 autoScrollEditorIntoView ={true}
-                value={``}
+              value={value}
                 setOptions={{
                   enableBasicAutocompletion: true,
                   enableLiveAutocompletion: true,
